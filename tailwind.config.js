9c272aec3baaab380871d5de81d61416e70fdd54
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-11-01 17:54:43 +0000

    ready for home page

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    // extend: {},
+    extend: {
+      // backgroundImage: {
+      //   gradient: "url('/static/background.png')",
+      // },
+      colors: {
+        purple: {
+          951: 'rgba(79, 70, 229, 1)',
+        },
+        gray: {
+          950: 'hsla(0, 0%, 100%, 0.7);',
+          951: 'hsla(0, 0%, 100%, 0.4);',
+          952: '#373b64',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
